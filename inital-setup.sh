5f53ce49c29125b44822b39bbad203bdf77096fb
Robin Lehrmann <robin.lehrmann@developer.nrw>
2019-02-03 16:38:45 +0100

    added owncloud

--- inital-setup.sh
+++ inital-setup.sh
@@ -2,8 +2,8 @@
 
 set -x
 
+echo "gpu_mem=16" >> /boot/config.txt
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" > /etc/apt/sources.list.d/ansible.list
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt-get update
-
