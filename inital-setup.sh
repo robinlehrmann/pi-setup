1f2b0470857efe83313d22b94db0d971761fa883
Robin Lehrmann <robin.lehrmann@developer.nrw>
2019-02-08 17:13:12 +0100

    added generated passwords

--- inital-setup.sh
+++ inital-setup.sh
@@ -7,3 +7,11 @@ echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" > /etc/ap
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt-get update
+
+cat <<EOF > inventory/prod/group_vars/pi.yml
+---
+mysql_password: `pwgen -sCB 15 1`
+pihole_password: `pwgen -sCB 15 1`
+owncloud_password: `pwgen -sCB 15 1`
+firefly_app_key: `pwgen -sCB 15 1`
+EOF
