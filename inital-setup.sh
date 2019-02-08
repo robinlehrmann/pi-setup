a5b58295c1debdd69e3f5c4c2acb190a32634a6f
Robin Lehrmann <robin.lehrmann@developer.nrw>
2019-02-08 18:55:52 +0100

    absoulte path in inital-setup.sh

--- inital-setup.sh
+++ inital-setup.sh
@@ -8,7 +8,7 @@ echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" > /etc/ap
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt-get update
 
-cat <<EOF > inventory/prod/group_vars/pi.yml
+cat <<EOF > /home/pi/setup/inventory/prod/group_vars/pi.yml
 ---
 mysql_password: `pwgen -sCB 15 1`
 pihole_password: `pwgen -sCB 15 1`
