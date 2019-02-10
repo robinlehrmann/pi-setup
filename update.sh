680ddc198ab03811baf40f33d00c2be8e14ad901
Robin Lehrmann <robin.lehrmann@developer.nrw>
2019-02-10 10:26:30 +0100

    added specific playbooks

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ set +x
 cd /home/pi/setup
 git pull origin master
 
-ansible-playbook --flush-cache -i inventory/prod/hosts site.yml
+ansible-playbook --flush-cache -i inventory/prod/hosts `hostname`.yml
 
 set -x
